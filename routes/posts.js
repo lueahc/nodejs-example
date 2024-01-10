1282d71a53bbc6dcf2f1dc5628e9b1ecb4ed7eee
BangChaeul <30cmlive@naver.com>
2024-01-10 21:37:53 +0900

    이미지 등록 API 추가

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,39 @@ const sequelize = require('../config/database');
 const HttpException = require('../HttpException');
 const asyncHandler = require('../utils/asyncHandler');
 
+const { S3Client } = require('@aws-sdk/client-s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+require('dotenv').config();
+
+
+
+// 이미지 등록
+const s3 = new S3Client({
+    credentials: {
+        region: process.env.AWS_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_S3_BUCKET,  //버킷명
+        // metadata: function (req, file, cb) {
+        //     cb(null, { fieldName: file.fieldname });
+        // },
+        key: function (req, file, cb) {
+            cb(null, file.originalname);
+        },
+    }),
+});
+
+router.post('/upload', upload.array('photos', 3), (req, res) => {
+    res.send(req.files);
+});
+
 
 
 // 게시글 작성
